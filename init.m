b7e43153a95864faa69e1661860d7beee3caf47c
ChengChengYang1997 <kb240608@gmail.com>
2020-12-03 01:20:03 +0800

    tune pid gains

--- init.m
+++ init.m
@@ -9,11 +9,11 @@ arm2.l = 1;
 arm2.m = 0.5;
 
 % pid gains
-pid1.p = 26;
-pid1.d = 65;
+pid1.p = 6;
+pid1.d = 9;
 pid1.i = 0.0;
 
-pid2.p = 15;
-pid2.d = 35;
+pid2.p = 12;
+pid2.d = 16;
 pid2.i = 0.2;
 
